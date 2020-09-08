802783ddeaf724d954d3be7a502d63ef9f581d86
TanyaTanyaG <geiger.tanya@yandex.ru>
2020-09-08 21:44:10 +0500

    package jm.task.core.jdbc.util; import java.sql.Connection; import java.sql.SQLException;
    @SuppressWarnings("ALL")
    public class Util {
        public Util() throws SQLException {
            String URL = "jdbc:mysql://localhost:3306";
            String loginName = "root";
            String password = "rootroot";
        }
    
        public static Connection getConnection() {
            return getConnection();
        }
    
    }

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+@SuppressWarnings("ALL")
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public Util() throws SQLException {
+        String URL = "jdbc:mysql://localhost:3306";
+        String loginName = "root";
+        String password = "rootroot";
+    }
+
+    public static Connection getConnection() {
+        return getConnection();
+    }
+
 }
