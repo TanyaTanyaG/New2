a4dd7fedfe052ec0a8bf3379db1bd1daffe6bcf9
TanyaTanyaG <geiger.tanya@yandex.ru>
2020-09-08 21:44:36 +0500

    package jm.task.core.jdbc;
    import jm.task.core.jdbc.model.User;
    import jm.task.core.jdbc.util.Util;
    
    import java.sql.ResultSet;
    import java.sql.SQLException;
    import java.sql.Statement;
    
    @SuppressWarnings("ALL")
    public class Main {
        public static void main(String[] args) throws SQLException {
            try {
                new Util();
            } catch (SQLException throwables) {
                throwables.printStackTrace();
            }
            String query = "select*from user";
    
                Statement statement= Util.getConnection().createStatement();
                ResultSet  resultSet = statement.executeQuery(query);
                while (resultSet.next()) {
                    User user = new User();
                 user.setId(resultSet.getLong(1));
                 user.setName(resultSet.getString(2));
                 user.setLastName(resultSet.getString(3));
                 user.setAge(resultSet.getByte(4));
                 System.out.println(user);
                }
    
            }
        }

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,34 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@SuppressWarnings("ALL")
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        try {
+            new Util();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        String query = "select*from user";
+
+
+            Statement statement= Util.getConnection().createStatement();
+            ResultSet  resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                User user = new User();
+             user.setId(resultSet.getLong(1));
+             user.setName(resultSet.getString(2));
+             user.setLastName(resultSet.getString(3));
+             user.setAge(resultSet.getByte(4));
+             System.out.println(user);
+            }
+
+        }
     }
-}
+
